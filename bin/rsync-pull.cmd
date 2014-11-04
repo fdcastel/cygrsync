61d02832d971e9890dc18c4ef078b95179004a6e
F.D.Castel <fdcastel@gmail.com>
2014-11-04 17:08:03 -0200

    Updates cygwin to 1.7.32. Minor fixes.
    Signed-off-by: F.D.Castel <fdcastel@gmail.com>

--- bin/rsync-pull.cmd
+++ bin/rsync-pull.cmd
@@ -12,18 +12,19 @@ FOR /f "eol=; tokens=* delims=" %%i IN ('cygpath.exe -u "%2"') DO (
 	SET CygwinFilename=%%i
 )
 
-rsync.exe --compress-level=9 --progress --human-readable "%1" "!CygwinFilename!"
+rsync.exe --compress-level=9 --progress --human-readable "%1" "!CygwinFilename!" %*
 GOTO :End
 
 
 :ShowUsage
-ECHO Send a file to remote server.
+ECHO Receive a file/folder from a remote server.
 ECHO.
-ECHO usage: %0 [remote-file] [local-file] 
+ECHO usage: %0 ^<remote-source^> ^<local-target^> [extra-args]
 ECHO.
 ECHO where
-ECHO      remote-file   remote file, in rsync format ^(HOST::/folder/file^)
-ECHO      local-file    file to write, in Windows format ^(C:\folder\file^)
+ECHO      remote-source   Source address in RSYNC format ^(HOST::/folder/file^).
+ECHO      local-target    Target file or folder in Windows format ^(C:\folder\file^)
+ECHO      extra-args      Extra arguments to rsync. ^(E.g. use '--recursive' for recursive transfers^)
 EXIT /B 1
 GOTO :End
 
--- bin/rsync-push.cmd
+++ bin/rsync-push.cmd
@@ -12,18 +12,19 @@ FOR /f "eol=; tokens=* delims=" %%i IN ('cygpath.exe -u "%1"') DO (
 	SET CygwinFilename=%%i
 )
 
-rsync.exe --compress-level=9 --progress --recursive --human-readable "!CygwinFilename!" %2
+rsync.exe --compress-level=9 --progress --human-readable "!CygwinFilename!" %2 %*
 GOTO :End
 
 
 :ShowUsage
-ECHO Send a file to remote server.
+ECHO Send a file/folder to a remote server.
 ECHO.
-ECHO usage: %0 [local-file] [remote-server] [extra-args]
+ECHO usage: %0 ^<local-source^> ^<remote-target^> [extra-args]
 ECHO.
 ECHO where
-ECHO      local-file      file to send
-ECHO      remote-server   target server
+ECHO      local-source    Source file or folder to send in WINDOWS format ^(C:\folder\file^).
+ECHO      remote-target   Target address in RSYNC format ^(HOST::/folder/file^).
+ECHO      extra-args      Extra arguments to rsync. ^(E.g. use '--recursive' for recursive transfers^)
 EXIT /B 1
 GOTO :End
 
